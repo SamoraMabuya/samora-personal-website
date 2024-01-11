b34ddf2bac35c58d0c9ce3ba103833b994687bf2
samora <samora.sm.sam@gmail.com>
2024-01-11 16:30:42 +0200

    sharp installed

--- next.config.js
+++ next.config.js
@@ -5,12 +5,6 @@ const nextConfig = {
     domains: ['personal-website-assets.nyc3.cdn.digitaloceanspaces.com.'],
     formats: ['image/avif', 'image/webp'],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  },
 }
 
 module.exports = nextConfig
