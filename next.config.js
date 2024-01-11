3d85d3e217df09f28b714139d7feaeb0b8dfc40a
samora <samora.sm.sam@gmail.com>
2024-01-11 16:46:10 +0200

    ignore errors during build

--- next.config.js
+++ next.config.js
@@ -5,6 +5,12 @@ const nextConfig = {
     domains: ['personal-website-assets.nyc3.cdn.digitaloceanspaces.com.'],
     formats: ['image/avif', 'image/webp'],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+  },
 }
 
 module.exports = nextConfig
