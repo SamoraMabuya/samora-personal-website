28b25f3b71da29b4fe222d351f3a222f8b04f34e
Samora Mabuya <samora.sm.sam@gmail.com>
2024-01-20 08:55:24 +0200

    next.config.js configured

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  images: {
-    domains: ['personal-website-assets.nyc3.cdn.digitaloceanspaces.com.'],
-    formats: ['image/avif', 'image/webp'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-    typescript: {
-      ignoreBuildErrors: true,
+    images: {
+        domains: ['personal-website-assets.nyc3.cdn.digitaloceanspaces.com.'],
+        formats: ['image/avif', 'image/webp'],
     },
-  },
+
 }
 
 module.exports = nextConfig
