a96d37a846784f4705324d98cc128b8aef0c88c7
samora <samora.sm.sam@gmail.com>
2024-01-31 19:29:46 +0200

    config: fixed nextjs config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
   images: {
-    domains: ['personal-website-assets.nyc3.cdn.digitaloceanspaces.com.'],
-    formats: ['image/avif', 'image/webp'],
+    domains: ["personal-website-assets.nyc3.cdn.digitaloceanspaces.com."],
+    formats: ["image/avif", "image/webp"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
-    typescript: {
-      ignoreBuildErrors: true,
-    },
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
